90a9b3eb898d2acc6b2fedcab3ed8b01581a6ec0
limoiie <limo.iie4@gmail.com>
2019-04-19 22:56:39 +0800

    add: New leetcode

---
+++ problems/leetcode041.h
@@ -0,0 +1,13 @@
+#ifndef OJ_LEETCODE041_H
+#define OJ_LEETCODE041_H
+
+#include "base.h"
+
+class SolutionLeetCode041 {
+public:
+    int firstMissingPositive(vector<int>& num) {
+
+    }
+};
+
+#endif //OJ_LEETCODE041_H
